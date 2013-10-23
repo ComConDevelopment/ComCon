c8d16b4f765825e9328d28a9230c7f4f9ddbdcc9
ComConDevelopment <comcondev@gmail.com>
2013-10-23 20:50:07 +0200

    Projekt neu erstellt
    Den Client Modular aufgebaut (Prism-Practices)

---
+++ ComCon ALT/ComConClient/ComConClient/ClientMainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ServiceModel;
+using ComConClient.MessageService;
+using ComCon.Shared.Classes;
+
+namespace ComConClient
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, IServerFunctionsCallback
+    {
+        ServerFunctionsClient client = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InstanceContext context = new InstanceContext(this);
+            client = new ServerFunctionsClient(context);
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            client.ConnectToServer("Test", "asdf");
+            
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            if (client.State == CommunicationState.Opened)
+            {
+                ChatMessage cm = new ChatMessage();
+                cm.Message = textBox2.Text;
+                cm.TimeStamp = DateTime.Today;
+                //cm.User = new ChatUser() { Username = "Test" };
+                client.Send(cm);
+            }
+            else
+            {
+                ShowMessage("Nicht eingeloggt!");
+            }
+        }
+
+        public void ShowMessage(string pMessage)
+        {
+            
+        }
+    }
+    
+}
---
+++ ComCon ALT/ComConClient/ComConServer/Views/ServerMainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ServiceModel;
+
+namespace ComConServer
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class ServerMainWindow : Window
+    {
+        ServiceHost sh = new ServiceHost(typeof(MessageService));
+
+        public ServerMainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                sh = new ServiceHost(typeof(MessageService));
+                sh.Open();
+                textBox1.AppendText("Server gestartet\n");
+            }
+            catch (Exception ex)
+            {
+                textBox1.AppendText(ex.Message);
+            }
+
+            
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            sh.Close();
+            textBox1.AppendText("Server gestoppt\n");
+        }
+    }
+}
