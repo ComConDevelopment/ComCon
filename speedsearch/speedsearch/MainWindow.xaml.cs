9bb406eb5f1736b72faed35f924b7fd73fda8289
ComConDevelopment <comcondev@gmail.com>
2014-01-09 10:41:24 +0100

    SpeedSearch (Metro Design)
    Ganz einfache Google Suche:
    -Suche eingeben und Enter
    -Alles im Metro Design
    -NuGet MahApps.Metro dazu genutzt

---
+++ speedsearch/speedsearch/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace speedsearch
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow 
+
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+     
+        public void tb_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+            if (e.Key == Key.Return)
+            {
+                try
+                {
+                    if (TextBox.Text != "")
+                    {
+                        Process.Start("http://google.de/search?q=" + TextBox.Text);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error");
+                }
+                TextBox.Text = "";
+            }
+          
+        }
+
+       
+  
+    }
+}
