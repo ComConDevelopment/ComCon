f7882ae7d3e4a6ff803cb0e20fd0d72ce0b977c4
ComConDevelopment <comcondev@gmail.com>
2014-01-21 15:01:09 +0100

    P42 von Tom

---
+++ P42_Tom/P42/P42/Models/ViewModelBase.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P42.Models
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        #region Deklaration
+
+
+        #endregion
+
+        #region Properties
+
+
+
+        #endregion
+
+        #region Konstruktor
+
+        public ViewModelBase()
+        {
+
+        }
+
+        #endregion
+
+        #region Events
+
+        public virtual void OverrideMe()
+        {
+
+        }
+
+        #endregion
+
+        #region Diverses
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void RaisePropertyChanged(string pPropertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(pPropertyName));
+            }
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
---
+++ P42_Tom/P42/P42/UserControls/GoogleControl.xaml.cs
@@ -0,0 +1,30 @@
+﻿using P42.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace P42.UserControls
+{
+    /// <summary>
+    /// Interaktionslogik für GoogleControl.xaml
+    /// </summary>
+    public partial class GoogleControl : UserControl
+    {
+        public GoogleControl()
+        {
+            InitializeComponent();
+            this.DataContext = new GoogleSearchModel();
+        }
+    }
+}
